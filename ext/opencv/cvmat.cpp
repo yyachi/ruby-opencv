24ea4cce15bb943f3984ad80caf0017b80a811ff
ser1zw <azariahsawtikes@gmail.com>
2012-09-25 18:26:42 +0900

    add document of CvMat#or

--- ext/opencv/cvmat.cpp
+++ ext/opencv/cvmat.cpp
@@ -1836,14 +1836,15 @@ rb_and(int argc, VALUE *argv, VALUE self)
 }
 
 /*
- * call-seq:
- *   or(<i>val[,mask]</i>) -> cvmat
+ * Calculates the per-element bit-wise disjunction of two arrays or an array and a scalar.
  *
- * Return new matrix computed per-element bit-wise disjunction.
- * <i>val</i> should be CvMat or CvScalar.
- * If <i>val</i> is CvMat, it must have same type (depth and channel).
- * For each element (I)
- *  dst(I) = src1(I) | src2(I) if mask(I) != 0
+ * @overload or(val, mask = nil)
+ * @param val [CvMat, CvScalar] Array or scalar to calculate bit-wise disjunction
+ * @param mask [CvMat] Optional operation mask, 8-bit single channel array, that specifies
+ *     elements of the destination array to be changed.
+ * @return [CvMat] Result array
+ * @opencv_func cvOr
+ * @opencv_func cvOrS
  */
 VALUE
 rb_or(int argc, VALUE *argv, VALUE self)
